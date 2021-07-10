005c2ae1ee77549fbf22fac4ef9567c7c9996d42
Chetna <>
2021-07-10 15:16:41 +0530

    first commit

---
+++ QuartzWithScopedServices/HelloCoderJob.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Quartz;
+using System;
+using System.Threading.Tasks;
+
+namespace QuartzWithScopedServices
+{
+
+    public class HelloCoderJob : IJob
+    {
+        private readonly ILogger<HelloCoderJob> _logger;
+
+        public HelloCoderJob(ILogger<HelloCoderJob> logger)
+        {
+            _logger = logger;
+        }
+
+        public Task Execute(IJobExecutionContext context)
+        {
+            _logger.LogInformation("Hello Coder!");
+            return Task.CompletedTask;
+        }
+    }
+}
